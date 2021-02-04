f2aaeaf1790098c380aa749445bc7da7fc3a771d
gauthier.cornec <gauthier.cornec@gmail.com>
2021-02-04 11:03:31 +0100

    ajout story 1

---
+++ story1.js
@@ -0,0 +1,37 @@
+const fs = require('fs'); // gestionnaire de fichier node (Create / read / Write / Copy / rename)
+//const { title } = require('process');
+// const { stringify } = require('querystring');
+//const { connect } = require('http2');
+const jsonFile = require  ('./movies2.json'); // acces to movies.json
+//const FileOut = require ('./movies.out.json')
+
+
+
+const moviesObjToString = JSON.stringify(jsonFile);
+//    console.log (typeof moviesObjToString);
+//    console.log(moviesObjToString);
+
+
+const toJsonMovies = JSON.parse(moviesObjToString);
+//    console.log(typeof toJsonMovies);
+//    console.log(toJsonMovies);
+
+var filmWithDateArray = [];
+jsonFile.forEach((film) => {
+    var release_date = film.release_date;
+//    console.log (release_date);
+
+    const filmReleaseDate = new Date(release_date * 1000);
+//   console.log(moonLanding.getFullYear()); 
+    film.title = film.title + " " + filmReleaseDate.getFullYear();
+    console.log (film);
+    filmWithDateArray.push(film);
+    }
+);
+
+const filmsFormated = JSON.stringify(filmWithDateArray);
+
+fs.writeFileSync('movies.out.json', filmsFormated);
+
+   
+// writefilessync
